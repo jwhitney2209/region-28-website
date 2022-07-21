9d1d51ad3c7e12e55a974d0a2753027dc3e0a465
teachergg <jwhitney@bisd.us>
2022-07-20 22:21:11 -0500

    added calendar page and download links

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import AnimatePage from "./AnimatePage";
+import calendar from '../../src/assets/calendar.png'
 
 const Calendar = () => {
   return (
     <AnimatePage>
-      <article className="notification is-info is-light m-4">
-        <div className="">
-          <strong>This page is currently under construction!</strong>
+      <article className="m-4">
+        <div className="mb-4 has-text-centered">
+          <a className="button is-danger is-outlined" href="https://drive.google.com/file/d/1w65skbr7DBAnRVbdOnWzzobN0B0YX5UO/view?usp=sharing" target="_blank" without rel="noreferrer">Download Calendar</a>
+        </div>
+        <div>
+          <img alt="calendar" src={calendar} />
           <br />
         </div>
       </article>
